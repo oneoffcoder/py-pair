9077f74aee91e36f476c6bba5cac5908003a0feb
Jee Vang, Ph.D <vangjee@gmail.com>
2020-11-25 19:49:17 -0500

    increment version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, One-Off Coder'
 author = 'Jee Vang'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.1'
+release = '3.0.2'
 
 
 # -- General configuration ---------------------------------------------------
