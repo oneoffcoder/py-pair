05398d53eb348eb2a9edf91ed883d360460642eb
Jee Vang, Ph.D <vangjee@gmail.com>
2021-09-30 14:17:22 -0400

    add test

--- tests/test_association.py
+++ tests/test_association.py
@@ -1,6 +1,8 @@
 import random
+from itertools import combinations
 
 import numpy as np
+import pandas as pd
 from nose import with_setup
 
 from pypair.association import binary_binary, categorical_categorical, \
@@ -144,3 +146,44 @@ def test_confusion():
     for m in ConfusionMatrix.measures():
         r = confusion(a, b, m)
         print(f'{r}: {m}')
+
+
+@with_setup(setup, teardown)
+def test_pandas_categorical():
+    """
+    Tests categorical correlation using Pandas dataframe ``.corr()``.
+
+    :return: None
+    """
+    # FIXME: pandas.corr() is broken; no longer supports non-numeric columns!
+    def get_associations(a, b):
+        d = {'x': a, 'y': b}
+        measures = {m: categorical_categorical(df[a], df[b], measure=m) for m in ['chisq', 'phi', 'mutual_information']}
+        d = {**d, **measures}
+        return d
+
+    df = pd.DataFrame({
+        'x1': ['on', 'on', 'on', 'on', 'on', 'off', 'off', 'off', 'off', 'off'],
+        'x2': ['on', 'off', 'on', 'off', 'on', 'off', 'on', 'off', 'on', 'off'],
+        'x3': ['off', 'off', 'off', 'off', 'off', 'on', 'on', 'on', 'on', 'on'],
+        'x4': ['on', 'on', 'on', 'on', 'off', 'off', 'off', 'off', 'off', 'on'],
+    })
+
+    associations = [get_associations(a, b) for a, b in combinations(df.columns, 2)]
+
+    expected = {
+        'chisq': [0.2857142857142857, 7.142857142857143, 2.5714285714285716, 0.2857142857142857, 0.2857142857142857,
+                  2.5714285714285716],
+        'phi': [0.14285714285714285, 0.7142857142857143, 0.4285714285714286, 0.14285714285714285, 0.14285714285714285,
+                0.4285714285714286],
+        'mutual_information': [0.010239075859473604, 0.2830308622715362, 0.09487759197468806, 0.010239075859473604,
+                               0.010239075859473604,
+                               0.09487759197468805]
+    }
+
+    a_df = pd.DataFrame(associations)
+
+    for field, e in expected.items():
+        o = list(a_df[field])
+        for v1, v2 in zip(o, e):
+            assert abs(v1 - v2) < 0.0001
